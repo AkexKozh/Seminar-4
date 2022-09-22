dfd14d59f621f0c996debb9f742630d175541991
AlexKozh <kojemiako-sascha@yandex.ru>
2022-09-22 14:57:37 +0700

    Solved Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine();
+
+int number = 5;
+int exponent = 2;
+int count = 1;
+int result = number;
+
+while (count < exponent)
+{
+    result = result * number;
+    count++;
+}
+
+Console.WriteLine($"{number} ^ {exponent} = {result}");
+Console.WriteLine();
